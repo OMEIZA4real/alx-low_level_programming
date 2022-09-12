d4cbaa8a2181935920ec58309eb83e665d8276d0
OMEIZA4real <omzy4oakland@gmail.com>
2022-09-11 21:24:40 -0700

    Task 101 Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,46 +5,7 @@
  * Return: 1
  */
 int main(void)
-
 {
-
 		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+		return (1);
 }
-
-
-
-
-
-Now for BETTY after cloning Betty and moving to /bin/
-
-touch testBetty.c
-
-#include <stdio.h>
-
-
-
-int main(void)
-
-{
-
-	int a = 5;
-
-	int b = 4;
-
-
-
-
-
-	for (a < b;;)
-
-	{
-
-		    printf("%d\n", a++);
-
-	}
-
-}
-
