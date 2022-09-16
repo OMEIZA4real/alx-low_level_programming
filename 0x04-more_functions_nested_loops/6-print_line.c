7bb90f73828f85ee94e96270c43881d39d44e95a
OMEIZA4real <omzy4oakland@gmail.com>
2022-09-16 12:50:21 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,36 +1,19 @@
-#include <stdio.h>                                            
+#include <stdio.h>
+#include "main.h"
 
-#include "main.h"                                             
+/**
+* print_line - prints a straight line
+* @n: parameter
+* Return:returns nothing
+*/
 
-                                                              
+void print_line(int n)
+{
 
-/**                                                           
+	while (n-- > 0)
+	{
 
-* print_line - prints a straight line                         
-
-* @n: parameter                                               
-
-* Return:returns nothing                                      
-*/                                                           
-
-                                                              
-
-void print_line(int n)                                        
-
-{                                                             
-
-                                                              
-
-        while (n-- > 0)                                       
-
-        {                                                     
-
-                                                              
-
-                putchar('_');                                 
-
-        }                                                     
-
-        putchar('\n');                                        
-
+		putchar('_');
+	}
+	putchar('\n');
 }
