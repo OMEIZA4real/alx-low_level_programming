7911277538decb92ddd3d9e88e67729022b4a47c
OMEIZA4real <omzy4oakland@gmail.com>
2022-09-16 15:21:05 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,8 +14,10 @@ void print_square(int size)
 
 	if (size > 0)
 	{
+
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
+
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
 			{
 
@@ -24,12 +26,14 @@ void print_square(int size)
 
 			putchar('#');
 			putchar('\n');
-		
+
 		}
 
 	}
 	else
 	{
+
 		putchar('\n');
 	}
+
 }
