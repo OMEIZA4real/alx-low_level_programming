6cb6532a65e9ee31e4b449488b3e4380ef99479f
OMEIZA4real <omzy4oakland@gmail.com>
2022-09-26 19:29:04 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,20 +3,20 @@
 /**
  * _memset - fill a block of memory
  * @s: starting address
- * @b: value 
- * @n: o of bytes 
+ * @b: value
+ * @n: o of bytes
  * Return: Always 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0; 
+	int i = 0;
 
-	for (; n > 0; I++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 
 	}
-	return (s);
+	return (s)
 
 }
