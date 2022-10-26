d2f533091d48eca0f3e9fc36c1935a95a2ee123e
OMEIZA4real <omzy4oakland@gmail.com>
2022-10-25 21:19:24 -0700

    task 12 quik fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,29 +3,6 @@
 #include <stdlib.h>
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-void free_listp(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
  * print_listint_safe - prints a singly linked list (can print lists with loop)
  * @head: head pointer of singly linked list
  *
