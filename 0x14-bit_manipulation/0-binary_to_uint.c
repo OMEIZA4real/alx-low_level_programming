0f9019b640d49170ed78dced14535f46968ee92c
OMEIZA4real <omzy4oakland@gmail.com>
2022-10-27 21:33:26 -0700

    task 0 update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int res = 0;
+	unsigned int res = 0, one, i;
+	int l = 0;
 
-	if (!b)
+	if (b == 0)
 		return (0);
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 1 * dec_val + (b[i] - '0');
-	}
-	return (dec_val);
 
+
+	while (b[l] != 0)
+	{
+		if (b[l] != '0' && b[l] != '1')
+			return (0);
+		l++;
+	}
+
+	l = l - 1;
+
+	for (i = 0; l >= 0; i++, l--)
+	{
+		if (b[l] == '0')
+			one = 0;
+		else if (b[l] == '1')
+			one = 1;
+		one = one << i;
+		res = res | one;
+	}
+	return (res);
 }
